169a61fbcd9bed68efa2f241dd84132d85bbcfbd
N0ks1_ <77494915+NKMKM@users.noreply.github.com>
2025-02-06 11:54:04 +0200

    loginpage fix

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ const LoginForm = () => {
     const handleLogin = async () => {
         setLoading(true); // Запускаем индикатор загрузки
         try {
-            const res = await fetch("http://localhost:5000/login", {
+            const res = await fetch("http://localhost:5000/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
