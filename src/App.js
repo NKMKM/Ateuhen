eee7317373460ff52a25a993cfeb51c3d16cb91c
N0ks1_ <77494915+NKMKM@users.noreply.github.com>
2025-02-06 21:09:23 +0200

    Fixed InfoPage, created some tasks for future

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import RegisterForm from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import InfoPage from './info_pages/Home';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -23,10 +24,11 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* "/" - сделанно, по исправлениям, убрать стрелочки слайдера(Sanyaaaa), добавить линк к некоторым кнопкам(N0ks1_) */}
+        <Route path="/" element={user ? <Navigate to="/home" /> : <InfoPage />} />
         <Route path="/register" element={user ? <Navigate to="/home" /> : <RegisterForm setUser={setUser} />} />
         <Route path="/login" element={user ? <Navigate to="/home" /> : <LoginPage setUser={setUser} />} />
-        <Route path="/home" element={user ? <HomePage user={user} setUser={setUser} /> : <Navigate to="/login" />} />
-        <Route path="/" element={<Navigate to={user ? "/home" : "/login"} />} />
+        <Route path="/home" element={user ? <HomePage user={user} setUser={setUser} /> : <Navigate to="/" />} />
       </Routes>
     </Router>
   );
