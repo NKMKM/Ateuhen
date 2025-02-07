063c0fa7bb9588ff319296c60ce2d1235875e776
slizis228 <benritov@gmail.com>
2025-02-07 21:52:21 +0200

    .

--- src/info_pages/Premium.jsx
+++ src/info_pages/Premium.jsx
@@ -8,7 +8,7 @@ const Premium = () => {
     <>
         <Nav/>
         <div className='relative flex flex-col items-center'>
-            <h1 className='absolute top-0 text-center text-[14em] font-bold -z-10'>Pricing</h1>
+            <h1 className='absolute top-0 text-center text-[14em] font-bold -z-10 text-[#8A3CFA]'>Pricing</h1>
             <div className='flex bottom-0 flex-row space-x-7 w-[85%] mx-auto  my-48 z-20 '>
                 <div className='bg-white/10 rounded-lg h-[700px] w-1/3 flex flex-col lift-on-hover backdrop-blur-lg '>
                     <div className='p-6'>
@@ -25,7 +25,7 @@ const Premium = () => {
                             <li className='flex flex-row items-center text-lg'><div className='inline-block w-[40px] h-[40px] mr-6'><IoIosCheckmarkCircle className='w-full h-full' /></div>Good free stuff</li>
                         </ul>
                     </div>
-                    <button className='w-[90%] h-[50px] mt-10 border-2 border-white rounded-lg mx-auto hover:scale-105 hover:bg-white hover:text-black hover:font-bold transition duration-300'>Get Started</button>
+                    <button className='w-[90%] h-[50px] mt-10 border-2 border-white rounded-lg mx-auto  button-violet-hover'>Get Started</button>
                 </div>
                 <div className='bg-white/10 rounded-lg h-[700px] w-1/3 flex flex-col lift-on-hover backdrop-blur-lg'>
                     <div className='p-6'>
@@ -42,7 +42,7 @@ const Premium = () => {
                             <li className='flex flex-row items-center text-lg'><div className='inline-block w-[40px] h-[40px] mr-6'><IoIosCheckmarkCircle className='w-full h-full' /></div>Good standart stuff</li>
                         </ul>
                     </div>
-                    <button className='w-[90%] h-[50px] mt-10 border-2 border-white rounded-lg mx-auto hover:scale-105 hover:bg-white hover:text-black hover:font-bold transition duration-300'>Get Started</button>
+                    <button className='w-[90%] h-[50px] mt-10 border-2 border-white rounded-lg mx-auto button-violet-hover'>Get Started</button>
                 </div>
                 <div className='bg-white/10 rounded-lg h-[700px] w-1/3 flex flex-col lift-on-hover backdrop-blur-lg'>
                     <div className='p-6'>
@@ -59,7 +59,7 @@ const Premium = () => {
                             <li className='flex flex-row items-center text-lg'><div className='inline-block w-[40px] h-[40px] mr-6'><IoIosCheckmarkCircle className='w-full h-full' /></div>Good rich stuff</li>
                         </ul>
                     </div>
-                    <button className='w-[90%] h-[50px] mt-10 border-2 border-white rounded-lg mx-auto hover:scale-105 hover:bg-white hover:text-black hover:font-bold transition duration-300'>Get Started</button>
+                    <button className='w-[90%] h-[50px] mt-10 border-2 border-white rounded-lg mx-auto button-violet-hover'>Get Started</button>
                 </div>
             </div>
         </div>
